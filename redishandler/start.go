ad4f7e3fc05c5607c367f7e379ded10480c47c90
Nikola Broyak <nikola.broyak@websensa.com>
2022-06-11 01:00:40 +0200

    Localhost 1000

--- redishandler/start.go
+++ redishandler/start.go
@@ -9,7 +9,7 @@ var Ctx = RDB.Context()
 
 func Start() *redis.Client {
 	RDB := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "localhost:1000",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
